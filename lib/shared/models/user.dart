c77f40c766cd19067af5bcda7ddafbe427c8ac1d
Rogerio Sobrinho <rogerioa.sobrinho@gmail.com>
2019-10-13 16:32:51 -0300

    change user model - included toJson and fromJson method

--- lib/shared/models/user.dart
+++ lib/shared/models/user.dart
@@ -1,11 +1,23 @@
 import 'package:equatable/equatable.dart';
 
 class UserModel extends Equatable {
-  final String email;
-  final String name;
+  String email;
+  String name;
 
   UserModel({this.email, this.name});
 
   @override
   List<Object> get props => null;
+
+  UserModel.fromJson(Map<String, dynamic> json) {
+    this.email = json['email'];
+    this.name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.email;
+    data['email'] = this.name;
+    return data;
+  }
 }
